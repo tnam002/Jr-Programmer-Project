0704714b8bda553110980723a61a5ee73892db87
tnam002 <41878689+tnam002@users.noreply.github.com>
2022-08-27 11:13:35 -0700

    don't know what this is update

--- Assets/Optimization/Scripts/OptimUnit.cs
+++ Assets/Optimization/Scripts/OptimUnit.cs
@@ -31,8 +31,12 @@ public class OptimUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Profiler.BeginSample("HandleTime()");
         HandleTime();
+        Profiler.EndSample();
 
+
+        Profiler.BeginSample("Rotation");
         var t = transform;
 
         if(transform.position.x <= 0)
@@ -44,9 +48,13 @@ public class OptimUnit : MonoBehaviour
             transform.Rotate(0,0, currentAngularVelocity * Time.deltaTime);
         else if(transform.position.z < 0)
             transform.Rotate(0,0, -currentAngularVelocity * Time.deltaTime);
-        
-        Move();
+        Profiler.EndSample();
 
+        Profiler.BeginSample("Movement");
+        Move();
+        Profiler.EndSample();
+
+        Profiler.BeginSample("Boundary Check");
         //check if we are moving away from the zone and invert velocity if this is the case
         if (transform.position.x > areaSize.x && currentVelocity.x > 0)
         {
@@ -69,6 +77,7 @@ public class OptimUnit : MonoBehaviour
             currentVelocity.z *= -1;
             PickNewVelocityChangeTime();
         }
+        Profiler.EndSample();
     }
 
 
@@ -96,19 +105,20 @@ public class OptimUnit : MonoBehaviour
 
     void Move()
     {
-        Vector3 position = transform.position;
+        transform.position += currentVelocity * Time.deltaTime;
+        //Vector3 position = transform.position;
+
+        //float distanceToCenter = Vector3.Distance(Vector3.zero, position);
+        //float speed = 0.5f + distanceToCenter / areaSize.magnitude;
         
-        float distanceToCenter = Vector3.Distance(Vector3.zero, position);
-        float speed = 0.5f + distanceToCenter / areaSize.magnitude;
+        //int steps = Random.Range(1000, 2000);
+        //float increment = Time.deltaTime / steps;
+        //for (int i = 0; i < steps; ++i)
+        //{
+        //    position += currentVelocity * increment * speed;
+        //}
         
-        int steps = Random.Range(1000, 2000);
-        float increment = Time.deltaTime / steps;
-        for (int i = 0; i < steps; ++i)
-        {
-            position += currentVelocity * increment * speed;
-        }
-        
-        transform.position = position;
+        //transform.position = position;
     }
 
     private void HandleTime()
--- Assets/Scripts/ResourcePile.cs
+++ Assets/Scripts/ResourcePile.cs
@@ -10,7 +10,20 @@ public class ResourcePile : Building
 {
     public ResourceItem Item;
 
-    public float ProductionSpeed = 0.5f;
+    private float m_ProductionSpeed = 0.5f;
+    public float ProductionSpeed
+    {
+        get { return m_ProductionSpeed; }
+        set {
+            if (value < 0)
+            {
+                Debug.Log("Negative value was attempted");
+            } else
+            {
+                m_ProductionSpeed = value;
+            }
+        }
+    }
 
     private float m_CurrentProduction = 0.0f;
 
@@ -26,13 +39,13 @@ public class ResourcePile : Building
         
         if (m_CurrentProduction < 1.0f)
         {
-            m_CurrentProduction += ProductionSpeed * Time.deltaTime;
+            m_CurrentProduction += m_ProductionSpeed * Time.deltaTime;
         }
     }
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
+        return $"Producing at the speed of {m_ProductionSpeed}/s";
         
     }
     
